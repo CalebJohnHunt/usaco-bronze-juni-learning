a7cba4b88925812561a1735704a9e14580472b06
Kaychub <kaychubyt@gmail.com>
2021-07-13 00:59:39 -0700

    Finished

--- 08-palindromic-squares/palsquare.cpp
+++ 08-palindromic-squares/palsquare.cpp
@@ -1,43 +1,60 @@
+/*
+ID: caleb.h1
+LANG: C++
+PROG: palsquare
+*/
 
-#include <math.h>
 #include <string>
-
+#include <fstream>
 #include <iostream>
 
-
-char asciiBaseToNum(char c) {
-    if ('0' <= c && c <= '9')
-        return c-48;
-    return c-55;
+// Map numbers to higher base digits
+// 0 -> 0, 5 -> 5
+// 10 -> a, 15 -> f
+char convertNumToDigit(char c) {
+    if (c < 10)
+        return c + '0';
+    return c - 10 + 'A';
 }
 
-char numToAsciiBase(char n, char base) {
-    if (n >= base) {
-        char ones = n % base;
-        char tens = n / base;
-    }
-}
-
-unsigned int baseToTen(std::string number, char base) {
-    unsigned int r = 0;
-    for (char i = number.length()-1; i >= 0; --i) {
-        char n = asciiBaseToNum(number[i]);
-        r += n * pow(base, number.length() - i -1);
-    }
-    return r;
-}
-
-std::string tenTobase(unsigned int number, char base) {
-    float l = log(number) / log(base);
-    for (float i = 0; i < l; ++i) {
-        unsigned int low = number % base;
+std::string tenToBase(unsigned int number, char base) {
+    std::string newNumber;
+    
+    while (number > 0) {
+        newNumber = convertNumToDigit(number % base) + newNumber;
         number /= base;
-        
     }
+
+    return newNumber;
 }
 
 
-int main() {
-    std::cout << baseToTen("100", 11) << std::endl;
+bool isPalindrome(std::string s) {
+    for (int i = 0; i < s.length() / 2; ++i) {
+        if (s[i] != s[s.length()-i-1])
+            return false;
+    }
+    return true;
+}
+
+
+int main(int argc, const char* argv[]) {
+
+    std::ifstream infile("palsquare.in");
+    int base;
+    infile >> base;
+    infile.close();
+
+    std::ofstream outfile("palsquare.out");
+    for (int i = 1; i < 301; ++i) {
+        int square = i * i;
+        std::string squareString = tenToBase(square, base);
+        // std::cout << i << ' ' << square << ' ' << squareString << std::endl;
+        if (isPalindrome(squareString))
+            outfile << tenToBase(i, base) << ' ' << squareString << std::endl;
+    }
+
+    outfile.close();
+
     return 0;
 }
