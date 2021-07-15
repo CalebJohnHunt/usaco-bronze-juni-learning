b4b5dd3e1dfd70a3dfcddc6dc404520bbe7f631d
Kaychub <kaychubyt@gmail.com>
2021-07-14 19:49:09 -0700

    Uses large array with prices as indices and quantity as value

---
+++ 11-mixing-milk/milk-no-map.cpp
@@ -0,0 +1,42 @@
+/*
+ID: caleb.h1
+LANG: C++
+PROG: milk
+*/
+
+#include <fstream>
+#include <iostream>
+
+#define MIN(a, b) a < b ? a : b
+#define MAX_FARMERS 1001
+
+int main() {
+    unsigned int milkToBuy, moneySpent, numOfFarmers, price, quantity, quantityAtPrice[MAX_FARMERS];
+    for (size_t i = 0; i < MAX_FARMERS; ++i) {
+        quantityAtPrice[i] = 0;
+    }
+
+    std::ifstream infile("milk.in");
+    infile >> milkToBuy >> numOfFarmers;
+
+    for (size_t i = 0; i < numOfFarmers; ++i) {
+        infile >> price >> quantity;
+        quantityAtPrice[price] += quantity;
+    }
+
+    infile.close();
+
+    price = 0;
+    while (milkToBuy > 0) {
+        quantity = MIN(milkToBuy, quantityAtPrice[price]);
+        milkToBuy -= quantity;
+        moneySpent += quantity * price;
+        ++price;
+    }
+
+    std::ofstream outfile("milk.out");
+    outfile << moneySpent << std::endl;
+    outfile.close();
+
+    return 0;
+}
